e0149a7a2923c5a8ee057efcbef7a42d8705255f
JuliusMatadamas <julius3877@gmail.com>
2024-02-03 19:06:17 -0600

    Se crean el routing inicial de la aplicacion.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
+    },
+    {
+        path: 'login',
+        loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule)
+    },
+    {
+        path: 'register',
+        loadChildren: () => import('./pages/register/register.module').then(m => m.RegisterModule)
+    },
+    {
+        path: '**',
+        pathMatch: 'full',
+        loadChildren: () => import('./pages/error404/error404.module').then(m => m.Error404Module)
+    }
+];
